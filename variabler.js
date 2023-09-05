d258e39e076e7e872cba5872345cf5c9e1827662
Timpan42 <tim@fagerdal.se>
2023-09-05 14:26:19 +0200

    variabler med saker

---
+++ variabler.js
@@ -0,0 +1,27 @@
+let name = "Tim Fagerdal"
+
+console.log(typeof(name))
+
+const greating = `Hej ${name}`
+const greating2 = 'Hej ' + name
+
+console.log(greating)
+console.log(greating2)
+
+let age = 19
+cl(typeof(age))
+
+let x = Number.MAX_VALUE
+cl(name + " " + x * 2)
+
+let y =x*2+1
+cl(typeof(y))
+
+isNaN(y)
+
+function cl (x){
+    console.log(x)
+}
+function clT(x){
+    console.log(typeof(x))
+}
